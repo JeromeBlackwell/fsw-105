19259c5b4dcd29db962401db259c9f0932ed467c
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2020-11-20 22:23:35 -0500

    week1

---
+++ week1/shopper.js
@@ -0,0 +1,11 @@
+var shopper = {
+    product: "Waygu Ribeye",
+    department: "Meat",
+    barcode: 8948430,
+    price: 139.47,
+    display: function () { 
+        console.log (shopper.product) + (shopper.department) + (shopper.barcode) + (shopper.price);
+        }
+    }
+
+    shopper.displayInfo();
