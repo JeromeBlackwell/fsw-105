364547eeeca17a3c340eabf761d0d83d79bf44fa
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2020-11-21 23:54:18 -0500

    Week1

--- week1/shopper.js
+++ week1/shopper.js
@@ -4,7 +4,7 @@ var shopper = {
     barcode: 8948430,
     price: 139.47,
     display: function () { 
-        console.log ((shopper.product) + (shopper.department) + (shopper.barcode) + (shopper.price));
+        // console.log ((shopper.product) + (shopper.department) + (shopper.barcode) + (shopper.price));
         console.log(`${shopper.product} ${shopper.department} ${shopper.barcode} ${shopper.price}`);
             } 
     }
