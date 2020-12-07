96554e470a57aa3c9fae2267a0223d8be23c6cb4
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2020-12-07 00:00:57 -0500

    Week3

--- week3/index.pp2.js
+++ week3/index.pp2.js
@@ -17,3 +17,14 @@ if(number % 2 == 0) {
 else {
     console.log("The number is odd.");
 }
+
+var fibonacci_series = function (n) {
+    if (n === 1)
+        return [1,1,2,3,5];
+    else {
+        var s = fibonacci_series (n-1);
+        s.push(s[s.length - 1] + s[s.length - 2]);
+        return s;
+    }
+}
+console.log(fibonacci_series(8));
