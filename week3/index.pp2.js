6d477af0248e752913f8293a4e17544ed8d84d88
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2020-12-07 00:19:14 -0500

    Week3

--- week3/index.pp2.js
+++ week3/index.pp2.js
@@ -20,7 +20,7 @@ else {
 
 var fibonacci_series = function (n) {
     if (n === 1)
-        return [1,1,2,3,5];
+        return [1,2,3,3,5];
     else {
         var s = fibonacci_series (n-1);
         s.push(s[s.length - 1] + s[s.length - 2]);
