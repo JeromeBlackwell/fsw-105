317e9ee5dd42bd2a405c218137f5675ca884be39
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2020-12-14 02:16:17 -0500

    week4

---
+++ week4/Project2/project.js
@@ -0,0 +1,20 @@
+const readlineSync = require ("readline-syc");
+const num1 = readlineSync.questionInt("provide first num")
+const num2 = readlineSync.questionInt("provide second num")
+var enteredOperator = readlineSync.question("what operation do you ant to perfor : add, sub, mul, div")
+
+function add(num1, num2){
+return "addition of number -1: " + num1 + " with number -2:: " + num2 + " returns a sum of: " + (num1 + num2);
+}
+if(enteredOperator == "add"){
+    console.log(add (num1, num2));
+}
+else if(enteredOperator == "sub"){
+    console.log(sub (num1, num2));
+}
+else if(enteredOperator == "mul"){
+    consule.log(mul (num1, num2));
+}
+else if(enteredOperator == "div"){
+    console.lof(div (num1, num2));
+}
