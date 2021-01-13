e955899ddeca0807223d81ef53e0d4ddaa482dbe
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2021-01-12 20:35:44 -0500

    week7

--- week7/jurassicpark.js
+++ week7/jurassicpark.js
@@ -5,7 +5,7 @@ var jurassicPark = function() {
     var self = this;
     var dinasour;
     var generateStraydinasour = function() {
-        var StrayInt = Math.floor(Math.Stray() * ((dinasours.length - 1) - 0 + 1)) + 0;
+        var StrayInt = Math.floor(Math.random() * ((dinasours.length - 1) - 0 + 1)) + 0;
         dinasour = dinasours[StrayInt];
         return dinasour;
     };
@@ -17,13 +17,16 @@ var jurassicPark = function() {
     this.dinasourCount = 1;
     this.userAction = function(){
         var action= readlineSync.question('What do you want to do?, enter "r" to run or "a" to attack; ').toLowerCase();
-        var userAttackPower = Math.floor(Math.stray() * (50 - 25 + 1) + 25);
+        var userAttackPower = Math.floor(Math.random() * (50 - 25 + 1) + 25);
         switch(action){
             case 'r':
+                console.log(' You\'re a scardy-cat for running away or are not killing me today ' + dinasour + '\n the world of Jurassic Park');
+                var random = Math.floor(Math.random() * 10)
+                console.log (random);
+                if (random <= 3){
                 self.userIsActive = false;
-                console.log('You\'re a scardy-cat for running away or are not killing me today ' + dinasour + '\n the world of Jurassic Park');
                 setTimeout(function(){
-                    console.log(':)');
+                console.log(':)');
                 }, 2000);
                 setTimeout(function(){
                     console.log(':|');
@@ -38,9 +41,10 @@ var jurassicPark = function() {
                     console.log('A hungry T-Rex has eaten you!');
                 }, 6000);
                 break;
+            } else {console.log(" You escaped!")}
             case 'a':
                 self.dinasourHp -= userAttackPower;
-                console.log('You just attacked ' + dinasour + ' for ' + userAttackPower + 'attack power');
+                console.log(' You just attacked ' + dinasour + ' for ' + userAttackPower + 'attack power');
                 break;
             default:
                 console.log('Please enter a valid key');
@@ -48,12 +52,12 @@ var jurassicPark = function() {
     }
 this.dinasourAction = function() {
     if(self.userIsActive === true && self.dinasourHp > 0) {
-        var dinasourAttackPower = Math.floor(Math.Stray() * (50 - 25 + 1) + 25);
+        var dinasourAttackPower = Math.floor(Math.random() * (50 - 25 + 1) + 25);
         self.userHp -= dinasourAttackPower;
         console.log(dinasour + ' just attacked you ' + dinasourAttackPower + 'attack power');
         if(self.userHp <= 0) {
             self.userIsActive = false;
-            console.log(dinasour + 'has killed' + self.user + 'nYou wiil be eatin by a herd of dinasours.');
+            console.log(dinasour + ' has killed' + self.user + ' nYou wiil be eatin by a herd of dinasours.');
         }
     } else if (self.dinasourHp <= 0) {
             self.dinasourIsActive = false;
@@ -75,23 +79,26 @@ this.processAttack = function() {
 };
 this.initialize = function() {
         self.dinasourIsActive = true;
-        readlineSync.keyIn('Press any key to walk: ');
+        const walking = readlineSync.keyIn('Press w to Walk: ');
+        console.log (walking);
+        if (walking === "w") {
         generateStraydinasour();
         console.log ('Walking...');
-        console.log('Look look look, A' + dinasour + 'has appeared');
+        console.log(' Look look look, A' + dinasour + ' has appeared');
         self.processAttack();
         while(self.dinasourIsActive === false && self.dinasourCount <= dinasours.length) {
             console.log('============================================================');
-            readlineSync.keyIn('Press any key to walk:');
+            readlineSync.keyIn('Press w to Walk:');
             generateStraydinasour();
             console.log('Walking...');
-            console.log('Look out, A '+ dinasour + ' has appeared');
+            console.log(' Look out, A '+ dinasour + ' has appeared');
             self.restoredinasour();
             self.processAttack();
         }
         if (self.dinasourIsActive === false) {
-            console.log(self.user + 'is the master of Jurassic Park!');
+            console.log(self.user + ' is the master of Jurassic Park!');
         }
+    }
     };
     this.initialize();
 };
